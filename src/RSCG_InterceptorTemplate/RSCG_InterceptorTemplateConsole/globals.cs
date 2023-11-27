a9445cf8933ad85813c593e4b2c7682d89b33ead
Andrei Ignat <ignatandrei@yahoo.com>
2023-11-27 21:02:13 +0200

    with automated build

--- src/RSCG_InterceptorTemplate/RSCG_InterceptorTemplateConsole/globals.cs
+++ src/RSCG_InterceptorTemplate/RSCG_InterceptorTemplateConsole/globals.cs
@@ -1,3 +1,1 @@
-﻿global using RSCG_InterceptorTemplateCommon;
-global using RSCG_DemoObjects;
-//[assembly: InterceptClassMethodsAttribute<Person>("personTemplate",nameof(Person.FullName))]
+﻿global using RSCG_DemoObjects;
